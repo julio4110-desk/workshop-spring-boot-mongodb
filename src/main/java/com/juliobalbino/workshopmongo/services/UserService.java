a2652519fa66b33bbbda9fe19d155b6950a2ab03
Julio Balbino <julio4110@hotmail.com>
2020-05-04 13:56:05 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/juliobalbino/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.juliobalbino.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.juliobalbino.workshopmongo.domain.User;
+import com.juliobalbino.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll() {
+		return repo.findAll();
+	}
+}
